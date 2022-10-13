25dba0a2f35f3c381e85dc5fb3b48a6196e72381
huangpeilin <huangpeilin@qunjielong.com>
2022-10-13 23:35:42 +0800

    feat: 增加一个文本的功能

--- src/miniprogram/components/rich-text-list/rich-text-list.ts
+++ src/miniprogram/components/rich-text-list/rich-text-list.ts
@@ -7,5 +7,20 @@ Component({
     },
     data: {
     },
-    methods: {},
+    methods: {
+        // 增加一个节点
+        handleAddItem(event: WXEvent){
+            const {index, type, value} = event.detail
+            console.log("handleAddItem", event);
+            const newList = this.data.list.slice()
+
+            newList.splice(index+1, 0, {
+                type,
+                value,
+            })
+            this.setData({
+                list: newList,
+            })
+        }
+    },
   });
