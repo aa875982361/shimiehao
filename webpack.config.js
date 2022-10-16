f93abbffd0ef026413456350e6d30267ed6cef1c
huangpeilin <huangpeilin@qunjielong.com>
2022-10-17 00:27:17 +0800

    feat: 修改构建方式为webpack 因为暂时没解决gulp-esbuild 的引用路径别名问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,94 @@
-const path = require('path');
-const { getWebpackEntryObjByMiniprogramRoot } = require('./webpack-plugins/utils');
+const { resolve } = require('path')
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MinaWebpackPlugin = require('./plugin/MinaWebpackPlugin')
+const MinaRuntimePlugin = require('./plugin/MinaRuntimePlugin')
+const LodashWebpackPlugin = require('lodash-webpack-plugin')
 
-const miniprogramRootPath = path.resolve("./src/miniprogram")
+const debuggable = process.env.BUILD_TYPE !== 'release'
 
 module.exports = {
-    context: miniprogramRootPath,
-    entry: getWebpackEntryObjByMiniprogramRoot(miniprogramRootPath),
-    output: {
-        path: path.resolve("dist"),
-        filename: "[name].js",
-    },
-    module: {
-        rules: [{
-        test: /\.tsx?$/,
+  context: resolve('src/miniprogram'),
+  entry: { main: './app.ts' },
+  output: {
+    path: resolve('dist'),
+    filename: '[name].js',
+    publicPath: resolve('dist'),
+    globalObject: 'wx',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+        "@core": resolve(__dirname, './src/miniprogram/core')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /src\/miniprogram\/(.*)\.(ts)x?$/,
+        exclude: /node_modules/,
         use: 'ts-loader',
-        exclude: /node_modules/
-        }]
+      },
+      {
+        test: /\.(less)$/,
+        include: /src\/miniprogram/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              name: '[path][name].wxss',
+              context: resolve('src/miniprogram'),
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                strictMath: true,
+              },
+            },
+          }
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: '**/*',
+          to: './',
+          filter: resourcePath => !['.ts', '.less'].some(item => resourcePath.endsWith(item)),
+        },
+      ],
+    }),
+    new MinaWebpackPlugin({
+      scriptExtensions: ['.ts', '.js'],
+      assetExtensions: ['.less'],
+    }),
+    new MinaRuntimePlugin(),
+    new LodashWebpackPlugin(),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV) || 'development',
+      BUILD_TYPE: JSON.stringify(process.env.BUILD_TYPE) || 'debug',
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: 'common',
+      minChunks: 2,
+      minSize: 0,
     },
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ]
+    runtimeChunk: {
+      name: 'runtime',
     },
-    mode: "development",
-    plugins: [
-    ]
-};
+  },
+  mode: debuggable ? 'none' : 'production',
+  devtool: debuggable ? 'inline-source-map' : 'source-map',
+}
